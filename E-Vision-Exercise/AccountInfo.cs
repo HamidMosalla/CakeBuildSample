19648084a9261ce8d8d3de8539a228353588cb8d
Hamid Mosalla <Xellarix@gmail.com>
2018-03-09 08:39:49 +0330

    initial commit

---
+++ E-Vision-Exercise/AccountInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using E_Vision_Exercise;
+
+namespace EVisionExercise
+{
+    public class AccountInfo
+    {
+        private readonly int _accountId;
+        private readonly IAccountService _accountService;
+        public AccountInfo(int accountId, IAccountService accountService)
+        {
+            _accountId = accountId;
+            _accountService = accountService;
+        }
+        public double Amount { get; private set; }
+        public void RefreshAmount()
+        {
+            Amount = _accountService.GetAccountAmount(_accountId);
+        }
+    }
+}
---
+++ E-Vision-Exercise/IAccountService.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace E_Vision_Exercise
+{
+    public interface IAccountService
+    {
+        double GetAccountAmount(int accountId);
+    }
+}
